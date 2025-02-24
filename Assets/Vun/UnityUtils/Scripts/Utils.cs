8b3985bff6b73e8102bd045fb88f92d156978512
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2025-02-24 17:18:26 +0700

    feat: Add ContainsFlag function
    Also fix some type

--- Assets/Vun/UnityUtils/Scripts/Utils.cs
+++ Assets/Vun/UnityUtils/Scripts/Utils.cs
@@ -22,7 +22,8 @@ namespace Vun.UnityUtils
         }
 
         /// <summary>
-        /// Get all values of enum <c>T</c>
+        /// Get all values of enum <c>T</c>.
+        /// Extension method version of <see cref="Enum.GetValues"/>
         /// </summary>
         /// <returns>Array of values of <c>T</c>. Never <c>null</c></returns>
         public static T[] GetValues<T>() where T : Enum
@@ -33,6 +34,21 @@ namespace Vun.UnityUtils
             }
 
             return Array.Empty<T>();
+        }
+
+        /// <summary>
+        /// A non-boxing version of <see cref="Enum.HasFlag"/>
+        /// </summary>
+        public static unsafe bool ContainsFlag<T>(this T container, T flag) where T : unmanaged, Enum
+        {
+            return sizeof(T) switch
+            {
+                1 => (*(byte*)&container & *(byte*)&flag) == *(byte*)&flag,
+                2 => (*(ushort*)&container & *(ushort*)&flag) == *(ushort*)&flag,
+                4 => (*(uint*)&container & *(uint*)&flag) == *(uint*)&flag,
+                8 => (*(ulong*)&container & *(ulong*)&flag) == *(ulong*)&flag,
+                _ => throw new ArgumentException("Invalid enum backing type")
+            };
         }
     }
 }
