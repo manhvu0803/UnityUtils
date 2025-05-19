41758878af39e04717cebfac70984f810752609c
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2025-05-19 15:51:52 +0700

    feat: Add native collections utils

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Unity Utils
 A collections of C# and Unity utility functions and classes
+# Installing
+Package Manager → Install from git URLs... → https://github.com/manhvu0803/UnityUtils.git
---
+++ Scripts/Extensions/NativeCollectionUtils.cs
@@ -0,0 +1,62 @@
+﻿#if UNITY_COLLECTION_EXISTS
+using System;
+using Unity.Collections;
+
+namespace Vun.UnityUtils
+{
+    public static class NativeCollectionUtils
+    {
+        public static void DisposeIfCreated<T>(this NativeList<T> list) where T : unmanaged
+        {
+            if (list.IsCreated)
+            {
+                list.Dispose();
+            }
+        }
+
+        public static void DisposeIfCreated<T>(this NativeArray<T> list) where T : unmanaged
+        {
+            if (list.IsCreated)
+            {
+                list.Dispose();
+            }
+        }
+
+        public static void DisposeIfCreated<T>(this NativeQueue<T> list) where T : unmanaged
+        {
+            if (list.IsCreated)
+            {
+                list.Dispose();
+            }
+        }
+
+        public static void DisposeIfCreated<T>(this NativeParallelHashSet<T> list) where T : unmanaged, IEquatable<T>
+        {
+            if (list.IsCreated)
+            {
+                list.Dispose();
+            }
+        }
+
+        public static void DisposeIfCreated<TKey, TValue>(this NativeParallelHashMap<TKey, TValue> list)
+            where TKey : unmanaged, IEquatable<TKey>
+            where TValue : struct
+        {
+            if (list.IsCreated)
+            {
+                list.Dispose();
+            }
+        }
+
+        public static void DisposeIfCreated<TKey, TValue>(this NativeParallelMultiHashMap<TKey, TValue> list)
+            where TKey : unmanaged, IEquatable<TKey>
+            where TValue : struct
+        {
+            if (list.IsCreated)
+            {
+                list.Dispose();
+            }
+        }
+    }
+}
+#endif
