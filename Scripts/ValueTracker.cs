5add603594ac8bc262bfc9f8ea65c8528bfad5ad
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2025-06-03 11:44:53 +0700

    feat: Remove Singleton, change ValueTracker constructor

--- Scripts/ValueTracker.cs
+++ Scripts/ValueTracker.cs
@@ -4,25 +4,23 @@ using System.Collections.Generic;
 namespace Vun.UnityUtils
 {
     /// <summary>
-    /// A tracker for a value that is needed to be keep track of over an interval.
-    /// Support max value cap to total amount per interval, interval between event broadcasts, and broadcast value threshold
+    /// A class for a value that is needed to be keep track of over an interval.
+    /// Support max value cap to total amount per interval, interval between event broadcasts, and broadcast value threshold.
+    /// Doesn't support negative values
     /// </summary>
     public class ValueTracker
     {
-        /// <summary>
-        /// Behaviour of <see cref="ValueTracker"/> when <see cref="ValueTracker.Accumulate"/> is called with an amount that exceed <see cref="ValueTracker.MaxTotalValuePerInterval"/>
-        /// </summary>
-        public enum MaxValueOption : byte
+        private static double IgnoreAddAmount(ValueTracker tracker, double amount)
         {
-            /// <summary>
-            /// Ignore the added amount
-            /// </summary>
-            Ignore,
+            return 0;
+        }
 
-            /// <summary>
-            /// Reduce the added amount so that <see cref="ValueTracker.AccumulatedValue"/> not exceed <see cref="ValueTracker.MaxTotalValuePerInterval"/>
-            /// </summary>
-            AddToCap
+        /// <summary>
+        /// Return a new add amount that will not exceed <see cref="MaxTotalValuePerInterval"/>
+        /// </summary>
+        public static double CapAddAmount(ValueTracker tracker, double amount)
+        {
+            return tracker.MaxTotalValuePerInterval - tracker.TotalValue;
         }
 
         private struct ValueAddedRecord
@@ -33,7 +31,7 @@ namespace Vun.UnityUtils
         }
 
         /// <summary>
-        /// Maximum <see cref="AccumulatedValue"/> between each <see cref="OnProcessingAccumulatedValue"/> broadcasts
+        /// Maximum <see cref="AccumulatedValue"/> during each <see cref="AccumulateInterval"/>
         /// </summary>
         public readonly double MaxTotalValuePerInterval;
 
@@ -47,9 +45,10 @@ namespace Vun.UnityUtils
         /// </summary>
         public readonly double MinBroadcastValue;
 
-        public readonly MaxValueOption MaxValueBehaviour;
-
-        private readonly Func<double> _timeGetter;
+        /// <summary>
+        /// The duration to keep track of accumulated values. In seconds
+        /// </summary>
+        public readonly double AccumulateInterval;
 
         /// <summary>
         /// Invoked in <see cref="Update"/> when the <see cref="MinBroadcastInterval"/> has passed and <see cref="MinBroadcastValue"/> is reached
@@ -58,29 +57,42 @@ namespace Vun.UnityUtils
 
         private readonly Queue<ValueAddedRecord> _recordQueue = new();
 
-        private double _totalValue;
-
         private double _lastBroadcastTime;
+
+        private readonly Func<double> _timeGetter;
+
+        private readonly Func<ValueTracker, double, double> _addAmountProcessor;
+
+        public double TotalValue { get; private set; }
 
         public double AccumulatedValue { get; private set; }
 
         /// <param name="timeGetter">Function to get the current timestamp</param>
+        /// <param name="accumulateInterval">See <see cref="AccumulateInterval"/></param>
         /// <param name="maxTotalValuePerInterval">See <see cref="MaxTotalValuePerInterval"/></param>
         /// <param name="minBroadcastInterval">See <see cref="MinBroadcastInterval"/></param>
         /// <param name="minBroadcastValue">See <see cref="MinBroadcastValue"/></param>
-        /// <param name="maxValueBehaviour">See <see cref="MaxValueOption"/></param>
+        /// <param name="maxValueBehaviour">See <see cref="MaxValueAction"/></param>
+        /// <param name="addAmountProcessor">
+        /// Function to process the add amount in case it exceed <see cref="MaxTotalValuePerInterval"/>.
+        /// The 1st param is this instance, the 2nd param is the add amount, return value is a new add amount.
+        /// If null, the add amount will be ignored.
+        /// <see cref="CapAddAmount"/> can be used here to reduce the add amount instead
+        /// </param>
         public ValueTracker(
             Func<double> timeGetter,
+            double accumulateInterval = 1,
             double maxTotalValuePerInterval = double.PositiveInfinity,
             double minBroadcastInterval = 1,
             double minBroadcastValue = 0,
-            MaxValueOption maxValueBehaviour = MaxValueOption.Ignore)
+            Func<ValueTracker, double, double> addAmountProcessor = null)
         {
             _timeGetter = timeGetter;
+            AccumulateInterval = accumulateInterval;
             MaxTotalValuePerInterval = maxTotalValuePerInterval;
             MinBroadcastInterval = minBroadcastInterval;
             MinBroadcastValue = minBroadcastValue;
-            MaxValueBehaviour = maxValueBehaviour;
+            _addAmountProcessor = addAmountProcessor ?? IgnoreAddAmount;
         }
 
         /// <summary>
@@ -88,29 +100,28 @@ namespace Vun.UnityUtils
         /// </summary>
         public void Accumulate(double amount)
         {
-            if (amount == 0 || _totalValue >= MaxTotalValuePerInterval)
+            if (TotalValue >= MaxTotalValuePerInterval)
             {
                 return;
             }
 
-            // Cap added amount to not exceed MaxTotalValuePerInterval
-            if (_totalValue + amount >= MaxTotalValuePerInterval)
+            if (amount + TotalValue > MaxTotalValuePerInterval)
             {
-                if (MaxValueBehaviour == MaxValueOption.Ignore)
-                {
-                    return;
-                }
-
-                amount = MaxTotalValuePerInterval - _totalValue;
+                amount = _addAmountProcessor.Invoke(this, amount);
             }
 
-            _totalValue += amount;
+            if (amount <= 0)
+            {
+                return;
+            }
+
+            TotalValue += amount;
             AccumulatedValue += amount;
 
             _recordQueue.Enqueue(new ValueAddedRecord
             {
                 Value = amount,
-                Timestamp = _timeGetter()
+                Timestamp = _timeGetter.Invoke()
             });
         }
 
@@ -126,13 +137,13 @@ namespace Vun.UnityUtils
 
             var currentTimestamp = _timeGetter();
             CheckAccumulatedAmount(currentTimestamp);
-            var boundary = currentTimestamp - 1;
+            var boundary = currentTimestamp - AccumulateInterval;
             var addEvent = _recordQueue.Peek();
 
             while (addEvent.Timestamp <= boundary && _recordQueue.Count > 0)
             {
                 _recordQueue.Dequeue();
-                _totalValue -= addEvent.Value;
+                TotalValue -= addEvent.Value;
 
                 if (_recordQueue.Count > 0)
                 {
@@ -143,7 +154,7 @@ namespace Vun.UnityUtils
 
         private void CheckAccumulatedAmount(double timestamp)
         {
-            if (AccumulatedValue <= MinBroadcastValue || timestamp - _lastBroadcastTime < MinBroadcastInterval)
+            if (AccumulatedValue < MinBroadcastValue || timestamp - _lastBroadcastTime < MinBroadcastInterval)
             {
                 return;
             }
@@ -153,11 +164,14 @@ namespace Vun.UnityUtils
             _lastBroadcastTime = timestamp;
         }
 
+        /// <summary>
+        /// Revert this tracker to before any value is accumulated
+        /// </summary>
         public void Reset()
         {
             _recordQueue.Clear();
             AccumulatedValue = 0;
-            _totalValue = 0;
+            TotalValue = 0;
         }
     }
 }
