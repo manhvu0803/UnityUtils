3a4a1f7dea3a8ddb8d6f2c856fa93652de9dd2ec
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2025-07-03 12:15:28 +0700

    feat: Add utilities for Color32

--- Scripts/Extensions/ColorUtils.cs
+++ Scripts/Extensions/ColorUtils.cs
@@ -4,6 +4,8 @@ namespace Vun.UnityUtils
 {
     public static class ColorUtils
     {
+        #region Color
+
         /// <summary>
         /// Return a new <see cref="Color"/> with set <see cref="Color.r"/> value
         /// </summary>
@@ -110,5 +112,118 @@ namespace Vun.UnityUtils
                 && Mathf.Abs(a.b - b.b) <= epsilon
                 && (!compareAlpha || Mathf.Abs(a.a - b.a) <= epsilon);
         }
+
+        #endregion
+
+        #region Color32
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.r"/> value
+        /// </summary>
+        public static Color32 WithRed(this Color32 color, byte r)
+        {
+            color.r = r;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.g"/> value
+        /// </summary>
+        public static Color32 WithGreen(this Color32 color, byte g)
+        {
+            color.g = g;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.b"/> value
+        /// </summary>
+        public static Color32 WithBlue(this Color32 color, byte b)
+        {
+            color.b = b;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.a"/> value
+        /// </summary>
+        public static Color32 WithAlpha(this Color32 color, byte a)
+        {
+            color.a = a;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.r"/> and <see cref="Color32.g"/> values
+        /// </summary>
+        public static Color32 WithRG(this Color32 color, byte r, byte g)
+        {
+            color.r = r;
+            color.g = g;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.r"/> and <see cref="Color32.b"/> values
+        /// </summary>
+        public static Color32 WithRB(this Color32 color, byte r, byte b)
+        {
+            color.r = r;
+            color.b = b;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.r"/> and <see cref="Color32.a"/> values
+        /// </summary>
+        public static Color32 WithRA(this Color32 color, byte r, byte a)
+        {
+            color.r = r;
+            color.a = a;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.g"/> and <see cref="Color32.b"/> values
+        /// </summary>
+        public static Color32 WithGB(this Color32 color, byte g, byte b)
+        {
+            color.g = g;
+            color.b = b;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.g"/> and <see cref="Color32.a"/> values
+        /// </summary>
+        public static Color32 WithGA(this Color32 color, byte g, byte a)
+        {
+            color.g = g;
+            color.a = a;
+            return color;
+        }
+
+        /// <summary>
+        /// Return a new <see cref="Color32"/> with set <see cref="Color32.b"/> and <see cref="Color32.a"/> values
+        /// </summary>
+        public static Color32 WithBA(this Color32 color, byte b, byte a)
+        {
+            color.b = b;
+            color.a = a;
+            return color;
+        }
+
+        /// <summary>
+        /// Compare 2 <see cref="Color32"/> by each RGB value, with optional alpha value
+        /// </summary>
+        public static bool Approximately(this in Color32 a, in Color32 b, byte epsilon = 0, bool compareAlpha = true)
+        {
+            return Mathf.Abs(a.r - b.r) <= epsilon
+                && Mathf.Abs(a.g - b.g) <= epsilon
+                && Mathf.Abs(a.b - b.b) <= epsilon
+                && (!compareAlpha || Mathf.Abs(a.a - b.a) <= epsilon);
+        }
+
+        #endregion
     }
 }
