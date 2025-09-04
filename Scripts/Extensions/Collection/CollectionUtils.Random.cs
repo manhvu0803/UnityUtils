3e16037b1c69b0a5b146dbd0d81fc7720c41179d
Nguyễn Mạnh Vũ <31726319+manhvu0803@users.noreply.github.com>
2025-09-04 12:02:48 +0700

    feat: Move random collections utils to new file

---
+++ Scripts/Extensions/Collection/CollectionUtils.Random.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vun.UnityUtils
+{
+    public partial class CollectionUtils
+    {
+        /// <summary>
+        /// Get a random element from <c>list</c>
+        /// </summary>
+        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
+        public static T RandomItem<T>(this IList<T> list)
+        {
+            return list.RandomItem<T, IList<T>>();
+        }
+
+        /// <summary>
+        /// Get a random element from <c>list</c>. A more complicated version of <see cref="RandomItem{T}(IList{T})"/> that avoids boxing
+        /// </summary>
+        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
+        public static T RandomItem<T, TList>(this TList list) where TList : IList<T>
+        {
+            return list is { Count: > 0 } ? list[UnityEngine.Random.Range(0, list.Count)] : default;
+        }
+
+        /// <summary>
+        /// Get a random element from <c>list</c>
+        /// </summary>
+        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
+        public static T RandomItem<T>(this IList<T> list, Random random)
+        {
+            return list.RandomItem<T, IList<T>>(random);
+        }
+
+        /// <summary>
+        /// Get a random element from <c>list</c>. A more complicated version of <see cref="RandomItem{T}"/> that avoids boxing
+        /// </summary>
+        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
+        public static T RandomItem<T, TList>(this TList list, Random random) where TList : IList<T>
+        {
+            return list is { Count: > 0 } ? list[random.Next(0, list.Count)] : default;
+        }
+
+#if UNITY_MATH_EXISTS
+        /// <summary>
+        /// Get a random element from <c>list</c>.
+        /// Will not change the <see cref="Unity.Mathematics.Random.state"/> of <c>random</c>
+        /// </summary>
+        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
+        public static T RandomItem<T>(this IList<T> list, Unity.Mathematics.Random random)
+        {
+            return list.RandomItem<T, IList<T>>(random);
+        }
+
+        /// <summary>
+        /// Get a random element from <c>list</c>.
+        /// Will change the <see cref="Unity.Mathematics.Random.state"/> of <c>random</c>
+        /// </summary>
+        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
+        public static T RandomItem<T>(this IList<T> list, ref Unity.Mathematics.Random random)
+        {
+            return list.RandomItem<T, IList<T>>(ref random);
+        }
+
+        /// <summary>
+        /// Get a random element from <c>list</c>.
+        /// Will not change the <see cref="Unity.Mathematics.Random.state"/> of <c>random</c>
+        /// </summary>
+        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
+        public static T RandomItem<T, TList>(this TList list, Unity.Mathematics.Random random) where TList : IList<T>
+        {
+            return list is { Count: > 0 } ? list[random.NextInt(0, list.Count)] : default;
+        }
+
+        /// <summary>
+        /// Get a random element from <c>list</c>.
+        /// Will change the <see cref="Unity.Mathematics.Random.state"/> of <c>random</c>
+        /// </summary>
+        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
+        public static T RandomItem<T, TList>(this TList list, ref Unity.Mathematics.Random random) where TList : IList<T>
+        {
+            return list is { Count: > 0 } ? list[random.NextInt(0, list.Count)] : default;
+        }
+#endif
+    }
+}
--- Scripts/Extensions/Collection/CollectionUtils.cs
+++ Scripts/Extensions/Collection/CollectionUtils.cs
@@ -48,42 +48,6 @@ namespace Vun.UnityUtils
         }
 
         /// <summary>
-        /// Get a random element from <c>list</c>
-        /// </summary>
-        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
-        public static T RandomItem<T>(this IList<T> list)
-        {
-            return list.RandomItem<T, IList<T>>();
-        }
-
-        /// <summary>
-        /// Get a random element from <c>list</c>. A more complicated version of <see cref="RandomItem{T}(IList{T})"/> that avoids boxing
-        /// </summary>
-        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
-        public static T RandomItem<T, TList>(this TList list) where TList : IList<T>
-        {
-            return list is { Count: > 0 } ? list[UnityEngine.Random.Range(0, list.Count)] : default;
-        }
-
-        /// <summary>
-        /// Get a random element from <c>list</c>
-        /// </summary>
-        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
-        public static T RandomItem<T>(this IList<T> list, Random random)
-        {
-            return list.RandomItem<T, IList<T>>(random);
-        }
-
-        /// <summary>
-        /// Get a random element from <c>list</c>. A more complicated version of <see cref="RandomItem{T}"/> that avoids boxing
-        /// </summary>
-        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
-        public static T RandomItem<T, TList>(this TList list, Random random) where TList : IList<T>
-        {
-            return list is { Count: > 0 } ? list[random.Next(0, list.Count)] : default;
-        }
-
-        /// <summary>
         /// Add <c>default</c> value or remove items from the end of <c>list</c>,
         /// until <see cref="IList.Count"/> is equal to <c>length</c>
         /// </summary>
@@ -291,47 +255,5 @@ namespace Vun.UnityUtils
         }
 
         #endregion
-
-#if UNITY_MATH_EXISTS
-        /// <summary>
-        /// Get a random element from <c>list</c>.
-        /// Will not change the <see cref="Unity.Mathematics.Random.state"/> of <c>random</c>
-        /// </summary>
-        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
-        public static T RandomItem<T>(this IList<T> list, Unity.Mathematics.Random random)
-        {
-            return list.RandomItem<T, IList<T>>(random);
-        }
-
-        /// <summary>
-        /// Get a random element from <c>list</c>.
-        /// Will change the <see cref="Unity.Mathematics.Random.state"/> of <c>random</c>
-        /// </summary>
-        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
-        public static T RandomItem<T>(this IList<T> list, ref Unity.Mathematics.Random random)
-        {
-            return list.RandomItem<T, IList<T>>(ref random);
-        }
-
-        /// <summary>
-        /// Get a random element from <c>list</c>.
-        /// Will not change the <see cref="Unity.Mathematics.Random.state"/> of <c>random</c>
-        /// </summary>
-        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
-        public static T RandomItem<T, TList>(this TList list, Unity.Mathematics.Random random) where TList : IList<T>
-        {
-            return list is { Count: > 0 } ? list[random.NextInt(0, list.Count)] : default;
-        }
-
-        /// <summary>
-        /// Get a random element from <c>list</c>.
-        /// Will change the <see cref="Unity.Mathematics.Random.state"/> of <c>random</c>
-        /// </summary>
-        /// <returns>A random element from <c>list</c>. <c>default</c> if <c>list</c> is <c>null</c> or empty </returns>
-        public static T RandomItem<T, TList>(this TList list, ref Unity.Mathematics.Random random) where TList : IList<T>
-        {
-            return list is { Count: > 0 } ? list[random.NextInt(0, list.Count)] : default;
-        }
-#endif
     }
 }
